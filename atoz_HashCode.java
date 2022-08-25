791773a82c56f8bf8eea95439e942a82c8b6bcf9
Karan_Mahajan <101745035+KaranMahajan19@users.noreply.github.com>
2022-08-25 20:11:48 +0530

    Add files via upload

---
+++ atoz_HashCode.java
@@ -0,0 +1,70 @@
+import java.lang.*;
+
+////////////////////////////////////////////////////////////////
+//
+//	Description : small letter "a" to "z" Hashcode
+//	Output		: Character
+//	Date		: 19/07/2022
+//	Author 		: Karan Mahajan
+//
+//////////////////////////////////////////////////////////////////
+
+class atoz_HashCode
+{
+	public static void main(String arr[])
+	{
+		String a = "a";
+		String b = "b";
+		String c = "c";
+		String d = "d";
+		String e = "e";
+		String f = "f";
+		String g = "g";
+		String h = "h";
+		String i = "i";
+		String j = "j";
+		String k = "k";
+		String l = "l";
+		String m = "m";
+		String n = "n";
+		String o = "o";
+		String p = "p";
+		String q = "q";
+		String r = "r";
+		String s = "s";
+		String t = "t";
+		String u = "u";
+		String v = "v";
+		String w = "w";
+		String x = "x";
+		String y = "y";
+		String z = "z";
+
+		System.out.println("HashCode Of 'a' : "+a.hashCode());
+		System.out.println("HashCode Of 'b' : "+b.hashCode());
+		System.out.println("HashCode Of 'c' : "+c.hashCode());
+		System.out.println("HashCode Of 'd' : "+d.hashCode());
+		System.out.println("HashCode Of 'e' : "+e.hashCode());
+		System.out.println("HashCode Of 'f' : "+f.hashCode());
+		System.out.println("HashCode Of 'g' : "+g.hashCode());
+		System.out.println("HashCode Of 'h' : "+h.hashCode());
+		System.out.println("HashCode Of 'i' : "+i.hashCode());
+		System.out.println("HashCode Of 'j' : "+j.hashCode());
+		System.out.println("HashCode Of 'k' : "+k.hashCode());
+		System.out.println("HashCode Of 'l' : "+l.hashCode());
+		System.out.println("HashCode Of 'm' : "+m.hashCode());
+		System.out.println("HashCode Of 'n' : "+n.hashCode());
+		System.out.println("HashCode Of 'o' : "+o.hashCode());
+		System.out.println("HashCode Of 'p' : "+p.hashCode());
+		System.out.println("HashCode Of 'q' : "+q.hashCode());
+		System.out.println("HashCode Of 'r' : "+r.hashCode());
+		System.out.println("HashCode Of 's' : "+s.hashCode());
+		System.out.println("HashCode Of 't' : "+t.hashCode());
+		System.out.println("HashCode Of 'u' : "+u.hashCode());
+		System.out.println("HashCode Of 'v' : "+v.hashCode());
+		System.out.println("HashCode Of 'w' : "+w.hashCode());
+		System.out.println("HashCode Of 'x' : "+x.hashCode());
+		System.out.println("HashCode Of 'y' : "+y.hashCode());
+		System.out.println("HashCode Of 'z' : "+z.hashCode());
+	}
+}
